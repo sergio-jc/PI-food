b02334cbb1392e3ade1b69aa2931c25a255c55e2
Your Namesergio-jc <sergiojara0609@gmail.com>
2022-06-06 11:57:58 -0500

    Cartas creadas

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import Recipes from './components/Recipes/Recipes';
+// import Recipes from './components/Recipes/Recipes';
+import Cards from './components/AllRecipes/AllRecipes.jsx';
 function App() {
   return (
     <div className="App">
       <h1>Henry Food</h1>
-      <Recipes/>
+      <Cards/>
     </div>
   );
 }
---
+++ client/src/components/AllRecipes/AllRecipes.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllRecipes } from "../../redux/action";
+
+const Cards = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllRecipes());
+  }, [dispatch]);
+
+  const allRecipes = useSelector((state) => state.recipes);
+
+  let display;
+  if (allRecipes) {
+    display = allRecipes.map(
+      ({id, name, image, healthScore, diets, dishTypes }) => {
+        return (
+            <>
+          <div key={id}>
+            <h3>{name}</h3>
+            <h3>{healthScore}</h3>
+            {diets.length && (
+              <ul>
+                DIETS:
+                {diets.map((e) => (
+                  <li>{e}</li>
+                ))}
+              </ul>
+            )}
+            {dishTypes.length && (
+              <ul>
+                DISH TYPES :
+                {dishTypes.map((e) => (
+                  <li>{e}</li>
+                ))}
+              </ul>
+            )}
+            <img src={image} alt="s" />
+          </div>
+            </>
+        );
+      }
+    );
+  } else {
+    display = "Recipes not found";
+  }
+  return <div>{display}</div>;
+};
+
+export default Cards;
