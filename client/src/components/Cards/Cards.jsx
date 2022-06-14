e995a6b6bdd0d5478005df605f086f6ea3d190ca
Your Namesergio-jc <sergiojara0609@gmail.com>
2022-06-13 21:39:05 -0500

    si se pudo eliminar al nino xd cards.jsx

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -1,34 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import "./CardsRecipes.css";
-const Cards = ({allRecipes , findRecipe}) => {
-  
-  let recipes = typeof(findRecipe) === 'string' ? allRecipes : findRecipe
-  let display;
-  if (recipes) {
-    display = recipes.map(({ id, name, image, healthScore, diets }) => {
-      return (
-        <Link to={`/detail/${id}`}>
-          <div key={`card_id_${id}`} className="card">
-            <h3 key={`name_id_${id}`}>{name}</h3>
-            <h2 key={`score_id_${id}`}>{healthScore}</h2>
-            <div>
-              <h3>Diets :</h3>
-              {diets.length && typeof diets[0] === "string"
-                ? diets.map((e, i) => <span key={`diet_id_${i}`}>{e}</span>)
-                : diets.map((e, i) => (
-                    <span key={`diet_id_${i}`}>{e.name}</span>
-                  ))}
-            </div>
-            <img key={`img_id_${id}`} src={image} alt="s" className="image" />
-          </div>
-        </Link>
-      );
-    });
+const Cards = () => {
+  const recipes = useSelector ((state) => state.recipes)
+  let display
+  if(typeof(recipes)==='object'){
+    display = recipes.map (({id,name,healthScore,diets,image},i)=>{
+      return(
+      <Link key={`card ${id}-${i}`} to={`/detail/${id}`}>
+      <div className="card" >
+        <h4>{name}</h4>
+        <h4>{healthScore}</h4>
+        <div>
+          <ul >
+            {diets && diets.map((e,i)=>{
+             return (<li key={`list ${i} card ${id}`}>{e}</li>)
+            })}
+          </ul>
+        </div>
+        <div>
+        <img src={image} alt="receta rica rica" />
+      </div>
+      </div>
+    </Link>
+    )})
   } else {
-    display = "Recipes not found";
+    display = <h1>Error</h1>
   }
-  return <div className="fund">{display}</div>;
+   return ( !recipes.length ?  <h1>loading</h1>  : <div className="fund">{display}</div>)
 };
 
 export default Cards;
