410588a196d0ceddec90bf097b1eea6aad873779
Your Namesergio-jc <sergiojara0609@gmail.com>
2022-06-15 01:31:25 -0500

    vamo que se puede terminar la landingpage

--- client/src/components/landingPage/landingPage.jsx
+++ client/src/components/landingPage/landingPage.jsx
@@ -2,18 +2,19 @@ import React from "react";
 import "../landingPage/landingPage.css";
 import { Link } from "react-router-dom";
 import Logo from '../landingPage/img1.png'
+import Logo2 from '../landingPage/img2.png'
 const LanndingPage = () => {
   return (
     <div className="backgorund_image">
       <section>
         <header>
           <img
-            src="https://freedesignfile.com/upload/2016/05/Healthy-eating-logo-design-vector-set-09.jpg"
+            src={Logo2}
             className="logo"
           />
         </header>
         <div className="content_landing">
-          <div className="textBox" />
+          <div className="textBox" >
           <h2>
             It's not just Recipes <br></br>It's <span>Healthy Eating</span>
           </h2>
@@ -24,8 +25,9 @@ const LanndingPage = () => {
             Assumenda voluptatibus accusantium atque illum!
           </p>
           <Link to={'/home'}>
-            <button>Home</button>
+            <div className="home_button">Home</div>
           </Link>
+          </div>
         </div>
         <div className="imgBox">
             <img src={Logo} className="starBucks"/>
