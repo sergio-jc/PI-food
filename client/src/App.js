5eb6610e60caa8c6a8e1f05faee70ad17518fc01
Your Namesergio-jc <sergiojara0609@gmail.com>
2022-06-17 00:38:39 -0500

    Error img

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import RecipeDetail from './components/RecipeDetail/RecipeDetail.jsx';
 import Home from './components/Home/Home.jsx';
 import LanndingPage from './components/landingPage/landingPage.jsx';
 import Loading from './components/Cards/Loading/Loading.jsx';
+import Error from './components/Cards/Error/Error.jsx';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Route path={'/form'} component={From}/>
       <Route path={'/detail/:id'} component={RecipeDetail}/>
       <Route path={'/lolo'} component={Loading}/>
+      <Route path={'/error'} component={Error}/>
     </BrowserRouter>
   );
 }
