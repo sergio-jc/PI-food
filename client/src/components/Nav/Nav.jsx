1500932c717c59c2ce2119b8ba6ecce3bc4888d1
Your Namesergio-jc <sergiojara0609@gmail.com>
2022-06-20 16:11:27 -0500

    vamo mejorando poco a poco

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -4,32 +4,41 @@ import SearchBar from "../SearchBar/SearchBar.jsx";
 import MinMax from "../Button/Order/MinMax.jsx";
 import Order from "../Button/Order/Order.jsx";
 import Filter from "../Button/Filter/Filter.jsx";
-import "../Nav/Nav.css"
-const Nav = ({ setCurrent , setLoader}) => {
+import Icon from '../landingPage/img2.png'
+import "../Nav/Nav.css";
+const Nav = ({ setCurrent, setLoader }) => {
   return (
     <div className="container">
+      <div className="icon" >
+        <img  src={Icon} alt="icono"/>
+      </div>
       <div className="nav_component">
-          <Link to="/" className='text'>🏠︎</Link>
-        </div>
-        <div className="nav_component">
-          <Link to="/home" className='text'>Home</Link>
-        </div>
-        <div className="nav_component">
-          <Link to={"/form"} className='text'>Form</Link>
-        </div>
-        <div className="nav_component">
-          <SearchBar  setLoader={ setLoader} setCurrent={setCurrent}/>
-        </div>
-        <div className="nav_component"> 
-          <Order />
-        </div>
-        <div className="nav_component">
-          <MinMax />
-        </div>
-        <div className="nav_component">
-          <Filter setCurrent={setCurrent} setLoader={setLoader}/>
-        </div>
-     
+        <Link to="/" className="text">
+          🏠︎
+        </Link>
+      </div>
+      {/* <div className="nav_component">
+        <Link to="/home" className="text">
+          Home
+        </Link>
+      </div> */}
+      <div className="nav_component">
+        <Link to={"/form"} className="text">
+          Form
+        </Link>
+      </div>
+      <div className="nav_component">
+        <SearchBar setLoader={setLoader} setCurrent={setCurrent} />
+      </div>
+      <div className="nav_component">
+        <Order />
+      </div>
+      <div className="nav_component">
+        <MinMax />
+      </div>
+      <div className="nav_component">
+        <Filter setCurrent={setCurrent} setLoader={setLoader} />
+      </div>
     </div>
   );
 };
