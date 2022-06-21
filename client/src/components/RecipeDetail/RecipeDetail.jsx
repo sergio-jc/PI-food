e5a48d9641c531a057f894b7e8bf48b432e7081a
Your Namesergio-jc <sergiojara0609@gmail.com>
2022-06-21 00:01:29 -0500

    boton home detail

--- client/src/components/RecipeDetail/RecipeDetail.jsx
+++ client/src/components/RecipeDetail/RecipeDetail.jsx
@@ -1,16 +1,16 @@
 import React, { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { clearDetail, Detail ,clearRecipes} from "../../redux/action";
-import Loading from "../Cards/Loading/Loading.jsx"
+import { clearDetail, Detail, clearRecipes } from "../../redux/action";
+import Loading from "../Cards/Loading/Loading.jsx";
 import "../RecipeDetail/Detail.css";
 const RecipeDetail = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
   useEffect(() => {
-    dispatch(clearRecipes())
+    dispatch(clearRecipes());
     dispatch(Detail(id));
-    return dispatch(clearDetail())
+    return dispatch(clearDetail());
   }, [dispatch, id]);
   const recipe = useSelector((state) => state.recipeDetail);
   const allDiets = recipe.diets
@@ -21,13 +21,16 @@ const RecipeDetail = () => {
         return <div key={`diets_${i}`}>{e.name}</div>;
       })
     : "don't find details";
-  return !recipe.image? <Loading/>  :(
+  return !recipe.image ? (
+    <Loading />
+  ) : (
     <div className="found_detail">
-      <div className="content_detail"> 
+      <div className="content_detail">
         <div className="img_score_diets">
           <div className="img_detail">
-            <Link to={'/home'}>
-            <img src={recipe.image} alt=" jkly" />
+            <Link to={"/home"}>
+              <div className="home_detail">🏠︎</div>
+              <img className=".img_detail_food" src={recipe.image} alt=" jkly" />
             </Link>
           </div>
           <div className="score_diets">
@@ -42,15 +45,14 @@ const RecipeDetail = () => {
 
         <div className="summary_steps">
           <div>
-          <div className="title_detail">
-            <div>{recipe.name}</div>
+            <div className="title_detail">
+              <div>{recipe.name}</div>
+            </div>
+
+            <div className="summary_contain">
+              <p>{recipe.summary}</p>
+            </div>
           </div>
-          
-          <div className="summary_contain">
-            <p>{recipe.summary}</p>
-          </div>
-          </div>
-          {/* <label>INSTRUCTIONS :</label> */}
           <div className="instruccions_detail">
             {recipe.analyzedInstructions?.split(" ✂ ").map((e, i) => (
               <div key={`step ${i}`} className="instruccions_detail">
@@ -61,7 +63,7 @@ const RecipeDetail = () => {
           <div>
             <div className="dish_contain">
               {" "}
-              DISH TYPES  : <span> {recipe.dishTypes}</span>
+              DISH TYPES : <span> {recipe.dishTypes}</span>
             </div>
           </div>
         </div>
