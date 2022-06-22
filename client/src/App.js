4c814f1b1073d4a0f98c9af817dcd497d5555e72
Your Namesergio-jc <sergiojara0609@gmail.com>
2022-06-21 21:26:06 -0500

    ala huajbak

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,6 @@ import {BrowserRouter} from 'react-router-dom'
 import RecipeDetail from './components/RecipeDetail/RecipeDetail.jsx';
 import Home from './components/Home/Home.jsx';
 import LanndingPage from './components/landingPage/landingPage.jsx';
-import Loading from './components/Cards/Loading/Loading.jsx';
-import Error from './components/Cards/Error/Error.jsx';
 
 function App() {
   return (
@@ -15,8 +13,6 @@ function App() {
       <Route path={'/home'} component={Home}/>
       <Route path={'/form'} component={From}/>
       <Route path={'/detail/:id'} component={RecipeDetail}/>
-      <Route path={'/lolo'} component={Loading}/>
-      <Route path={'/error'} component={Error}/>
     </BrowserRouter>
   );
 }
